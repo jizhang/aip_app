834df70ed220afa4b8dd5d1d8bce128529ee04cd
Ji ZHANG <zhangji87@gmail.com>
2020-01-31 14:09:44 +0800

    swipe pages

--- app/src/main/java/com/shzhangji/invest/FundViewActivity.java
+++ app/src/main/java/com/shzhangji/invest/FundViewActivity.java
@@ -1,8 +1,16 @@
 package com.shzhangji.invest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class FundViewActivity extends AppCompatActivity {
     @Override
@@ -12,5 +20,74 @@ public class FundViewActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("博时标普500ETF联接");
         getSupportActionBar().setSubtitle("050025");
+
+        ViewPager2 pager = findViewById(R.id.fund_view_pager);
+        pager.setAdapter(new PagerAdapter(this));
+    }
+
+    public static class PagerAdapter extends FragmentStateAdapter {
+        public PagerAdapter(FragmentActivity activity) {
+            super(activity);
+        }
+
+        @Override @NonNull
+        public Fragment createFragment(int position) {
+            Fragment fragment;
+            switch (position) {
+                case 0:
+                    fragment = new NetValueChartFragment();
+                    break;
+                case 1:
+                    fragment = new ProfitChartFragment();
+                    break;
+                case 2:
+                    fragment = new RecordListFragment();
+                    break;
+                default:
+                    throw new IllegalArgumentException("invalid position " + position);
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+
+    public static class NetValueChartFragment extends Fragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_fund_view_net_value_chart, container, false);
+        }
+    }
+
+    public static class ProfitChartFragment extends Fragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_fund_view_profit_chart, container, false);
+        }
+    }
+
+    public static class RecordListFragment extends Fragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_fund_view_record_list, container, false);
+        }
     }
 }
