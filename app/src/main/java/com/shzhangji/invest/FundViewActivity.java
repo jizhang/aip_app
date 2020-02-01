6e3478c23106ae1593d04897114bda852f9371aa
Ji ZHANG <zhangji87@gmail.com>
2020-02-01 16:50:53 +0800

    material tabs & toggle buttons

--- app/src/main/java/com/shzhangji/invest/FundViewActivity.java
+++ app/src/main/java/com/shzhangji/invest/FundViewActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class FundViewActivity extends AppCompatActivity {
     @Override
@@ -21,8 +27,25 @@ public class FundViewActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("博时标普500ETF联接");
         getSupportActionBar().setSubtitle("050025");
 
-        ViewPager2 pager = findViewById(R.id.fund_view_pager);
-        pager.setAdapter(new PagerAdapter(this));
+        ViewPager2 viewPager = findViewById(R.id.fund_view_pager);
+        viewPager.setAdapter(new PagerAdapter(this));
+
+        TabLayout tabLayout = findViewById(R.id.fund_view_tabs);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText(R.string.fund_view_text_net_value_chart);
+                    break;
+                case 1:
+                    tab.setText(R.string.fund_view_text_profit_chart);
+                    break;
+                case 2:
+                    tab.setText(R.string.fund_view_text_record_list);
+                    break;
+                default:
+                    throw new IllegalArgumentException("invalid position " + position);
+            }
+        }).attach();
     }
 
     public static class PagerAdapter extends FragmentStateAdapter {
@@ -63,7 +86,36 @@ public class FundViewActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_fund_view_net_value_chart, container, false);
+            View view = inflater.inflate(R.layout.fragment_fund_view_net_value_chart, container, false);
+            TextView holder = view.findViewById(R.id.fund_view_holder_net_value_chart);
+            MaterialButtonToggleGroup toggleGroup = view.findViewById(R.id.fund_view_button_periods);
+            toggleGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+                if (!isChecked) {
+                    return;
+                }
+                int period;
+                switch (checkedId) {
+                    case R.id.fund_view_button_period_3_months:
+                        period = R.string.fund_view_button_period_3_months;
+                        break;
+                    case R.id.fund_view_button_period_6_months:
+                        period = R.string.fund_view_button_period_6_months;
+                        break;
+                    case R.id.fund_view_button_period_1_year:
+                        period = R.string.fund_view_button_period_1_year;
+                        break;
+                    case R.id.fund_view_button_period_3_years:
+                        period = R.string.fund_view_button_period_3_years;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("invalid id " + checkedId);
+                }
+                holder.setText(getString(R.string.fund_view_text_holder,
+                        getString(period),
+                        getString(R.string.fund_view_text_net_value_chart)));
+            });
+            toggleGroup.check(R.id.fund_view_button_period_3_months);
+            return view;
         }
     }
 
@@ -75,7 +127,36 @@ public class FundViewActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_fund_view_profit_chart, container, false);
+            View view = inflater.inflate(R.layout.fragment_fund_view_profit_chart, container, false);
+            TextView holder = view.findViewById(R.id.fund_view_holder_profit_chart);
+            MaterialButtonToggleGroup toggleGroup = view.findViewById(R.id.fund_view_button_periods);
+            toggleGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+                if (!isChecked) {
+                    return;
+                }
+                int period;
+                switch (checkedId) {
+                    case R.id.fund_view_button_period_3_months:
+                        period = R.string.fund_view_button_period_3_months;
+                        break;
+                    case R.id.fund_view_button_period_6_months:
+                        period = R.string.fund_view_button_period_6_months;
+                        break;
+                    case R.id.fund_view_button_period_1_year:
+                        period = R.string.fund_view_button_period_1_year;
+                        break;
+                    case R.id.fund_view_button_period_3_years:
+                        period = R.string.fund_view_button_period_3_years;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("invalid id " + checkedId);
+                }
+                holder.setText(getString(R.string.fund_view_text_holder,
+                        getString(period),
+                        getString(R.string.fund_view_text_profit_chart)));
+            });
+            toggleGroup.check(R.id.fund_view_button_period_3_months);
+            return view;
         }
     }
 
